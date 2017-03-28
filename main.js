1ac578bea767cf8d2530480f4fff1f184f9fb2ca
Jakob Willforss <jakob.willforss@hotmail.com>
2017-03-28 18:39:02 +0200

    Random people spawning in place

--- main.js
+++ main.js
@@ -15,6 +15,9 @@ var frame_counter;
 var boundary_margin;
 var background_speed;
 
+var default_people_creation_speed = 38;
+var people_creation_speed = default_people_creation_speed;
+
 
 window.onload = function() {
 
@@ -73,6 +76,8 @@ function initialize_game() {
     boundary_margin = 20;
     background_speed = 10;
     objects = [];
+    people_creation_speed = parseInt(people_creation_speed / 2);
+    console.log(people_creation_speed);
 
     background.loadLevel("level2", objects);
 
@@ -88,6 +93,8 @@ function update() {
 
 	biker_object.update(rightPressed, leftPressed, upPressed, downPressed);
 	background.updateLevel(objects);
+
+	updateRandomPersonSpawn(frame_counter);
 
 	for (var i = 0; i < objects.length; i++){
 		var current_object = objects[i];
@@ -130,23 +137,28 @@ function update() {
 }
 
 
-function updateRandomSpawn(frame_counter) {
-		if (frame_counter % 30 === 0){
-		for (var i = 0; i < 1; i++){
-	       var x_position = Math.random()*(c.width-50);
-	       var y_position = c.height;
-	       objects.push(new Tree(c, x_position, y_position));
-	   }
-	}
-	if (frame_counter % 37 === 0){
-	    for (var i = 0; i < 1; i++){
-	        var x_position = Math.random()*(c.width-50);
-	        var y_position = c.height;
-	        x_position = Math.random()*(c.width-50);
-	        objects.push(new Walker(c, x_position, y_position))
-	    }
-	}
+function updateRandomPersonSpawn(frame_counter) {
+    if (frame_counter % people_creation_speed === 0){
+        for (var i = 0; i < 1; i++){
+            var x_position = Math.random()*(c.width-50);
+            var y_position = c.height;
+            x_position = Math.random()*(c.width-50);
+            objects.push(new Walker(c, x_position, y_position))
+        }
+    }
 }
+
+
+function updateRandomTreeSpawn(frame_counter) {
+    if (frame_counter % 30 === 0){
+        for (var i = 0; i < 1; i++){
+            var x_position = Math.random()*(c.width-50);
+            var y_position = c.height;
+            objects.push(new Tree(c, x_position, y_position));
+        }
+    }
+}
+
 
 // treats objects as perfect rectangles
 function collision(){
